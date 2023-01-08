b89607a0e9ebe78ce5727e3de3138d443df8a13f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-08 23:25:41 +0100

    feat: add new elements and styling

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,7 +4,12 @@ import logo from '../img/logo.png';
 const Menu = () => {
   return (
     <Container>
-      Menu
+      <Wrapper>
+        <Logo>
+          <Img src={logo} alt='logo' />
+          Youtube
+        </Logo>
+      </Wrapper>
     </Container>
   );
 };
@@ -14,6 +19,20 @@ const Container = styled.div`
   height: 100vh;
   background-color: #202020;
   color: #fff;
+`;
+
+const Wrapper = styled.div`
+  padding: 1.8rem 2.6rem;
+`;
+
+const Logo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+const Img = styled.img`
+  height: 2.5rem;
 `;
 
 export default Menu;
