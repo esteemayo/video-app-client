4c490e7ace7eaf2bdee99aef59413929475033fa
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-08 18:34:51 +0100

    feat: add sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 
 import Navbar from 'components/Navbar';
+import Sidebar from 'components/Sidebar';
 
 function App() {
   return (
     <Container>
       <Navbar />
+      <Main>
+        <Sidebar />
+      </Main>
     </Container>
   );
 }
