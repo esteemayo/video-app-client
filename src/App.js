b7f96951ecaa677a2446b22477ca7d651713c557
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-08 18:42:05 +0100

    fix: delete sidebar component

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 import Menu from 'components/Menu';
 import Navbar from 'components/Navbar';
-import Sidebar from 'components/Sidebar';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Menu />
       <Main>
         <Navbar />
-        <Sidebar />
         <Wrapper>
           cards
         </Wrapper>
