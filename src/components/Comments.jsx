93c51908396481c9baf0f520047e04918e09939b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-09 16:02:25 +0100

    feat: add css styles

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,8 +2,40 @@ import styled from 'styled-components';
 
 const Comments = () => {
   return (
-    <div>Comments</div>
+    <Container>
+      <NewComment>
+        <Avatar
+          src='https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo'
+          alt=''
+        />
+        <Input placeholder='Add a comment...' />
+      </NewComment>
+    </Container>
   );
 };
+
+const Container = styled.div``;
+
+const NewComment = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+const Avatar = styled.img`
+  width: 5rem;
+  height: 5rem;
+  border-radius: 50%;
+`;
+
+const Input = styled.input`
+  border: none;
+  outline: none;
+  width: 100%;
+  border-bottom: 1px solid ${({ theme }) => theme.soft};
+  background-color: transparent;
+  padding: 0.5rem;
+  caret-color: ${({ theme }) => theme.textSoft};
+`;
 
 export default Comments;
