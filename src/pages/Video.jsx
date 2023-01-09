b6b5c77e116b9dab17109f3de1f2535566278f73
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-09 22:15:33 +0100

    fix: modify

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -19,7 +19,7 @@ const Video = () => {
             title='YouTube video player'
             frameBorder='0'
             allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-            allowfullscreen
+            allowFullScreen
           ></iframe>
         </VideoWrapper>
         <Title>Test Video</Title>
