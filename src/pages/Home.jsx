95fa5ae6f47ad801571dccc5bfb4ab70a7df41c3
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-09 23:17:11 +0100

    fix: import react toastify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import styled from 'styled-components';
+import { toast } from 'react-toastify';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Card from 'components/Card';
