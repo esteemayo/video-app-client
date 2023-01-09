f7454f15f34f92b8dfab6d649c15bfab1824a6ef
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-09 22:27:03 +0100

    feat: handle fetch random videos api call

--- src/features/video/videoSlice.js
+++ src/features/video/videoSlice.js
@@ -1,4 +1,17 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getRandomVideos } from 'services/videoService';
+
+const fetchRandomVideos = createAsyncThunk(
+  'videos/getRandomVideos',
+  async (_, { rejectWithValue }) => {
+    try {
+      const { data } = await getRandomVideos();
+      console.log(data);
+    } catch (err) {
+      const message = err.response.data;
+      return rejectWithValue(message);
+    }
+  });
 
 const initialState = {
   videos: [],
@@ -11,6 +24,27 @@ const initialState = {
 export const videoSlice = createSlice({
   name: 'videos',
   initialState,
+  reducers: {
+    reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchRandomVideos.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchRandomVideos.fulfilled, (state, { payload }) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.videos.push(payload);
+      })
+      .addCase(fetchRandomVideos.rejected, (state, { payload }) => {
+        state.isLoading = false
+        state.isSuccess = false;
+        state.isError = payload;
+      })
+  }
 });
+
+export const { reset } = videoSlice.actions;
 
 export default videoSlice.reducer;
