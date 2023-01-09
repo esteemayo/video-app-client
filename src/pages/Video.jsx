3d9f1919a676b3dca1cfd8e6d7ca6b37eec0002f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-09 04:39:20 +0100

    feat: import mui icons

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,4 +1,8 @@
 import styled from 'styled-components';
+import ReplyOutlinedIcon from "@mui/icons-material/ReplyOutlined";
+import AddTaskOutlinedIcon from "@mui/icons-material/AddTaskOutlined";
+import ThumbUpOutlinedIcon from "@mui/icons-material/ThumbUpOutlined";
+import ThumbDownOffAltOutlinedIcon from "@mui/icons-material/ThumbDownOffAltOutlined";
 
 const Video = () => {
   return (
