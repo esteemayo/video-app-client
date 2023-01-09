15332d4116bc36be2c57d91c22c1f5246385bb62
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-09 17:42:58 +0100

    fix: create http service for axios configuration

---
+++ src/services/httpService.js
@@ -0,0 +1,45 @@
+import Axios from 'axios';
+import { toast } from 'react-toastify';
+
+import logger from './logService';
+
+const devEnv = process.env.NODE_ENV !== 'production';
+const { REACT_APP_API_DEV_URL, REACT_APP_API_PROD_URL } = process.env;
+
+const authFetch = Axios.create({
+  baseURL: devEnv ? REACT_APP_API_DEV_URL : REACT_APP_API_PROD_URL,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  },
+});
+
+authFetch.interceptors.request.use(
+  (req) => {
+    return req;
+  },
+  (error) => {
+    logger.log(error);
+    return Promise.reject(error);
+  });
+
+authFetch.interceptors.response.use(null, (error) => {
+  const expectedError = error.response &&
+    error.response.status >= 400 &&
+    error.response &&
+    error.response.status < 500;
+
+  if (!expectedError) {
+    toast.error('An unexpected error occurred');
+    return Promise.reject(error);
+  }
+});
+
+const http = {
+  get: authFetch.get,
+  post: authFetch.post,
+  patch: authFetch.patch,
+  delete: authFetch.delete,
+};
+
+export default http;
