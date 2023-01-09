be4a335fb7b7edbae9c48dad404adfb5bb366ba4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-09 16:55:46 +0100

    fix: passtype prop

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -12,6 +12,7 @@ const Card = ({ type }) => {
         />
         <Details type={type}>
           <ChannelImage
+            type={type}
             src='https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo'
             alt=''
           />
