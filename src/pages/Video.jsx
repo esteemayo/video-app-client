3f9c79c0e02f215366614448343b472a791ff20b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-09 15:35:27 +0100

    fix: modify

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -40,7 +40,7 @@ const Video = () => {
         <Hr />
         <Channel>
           <ChannelInfo>
-            <Image src="https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo" />
+            <Image src='https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo' />
             <ChannelDetail>
               <ChannelName>Lama Dev</ChannelName>
               <ChannelCounter>200K subscribers</ChannelCounter>
