33035970a281bbbfee3e449e3d0428e5c6f9bd7a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-09 02:36:57 +0100

    feat: import styled components, react router and icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,8 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
+
 const Navbar = () => {
   return (
     <div>Navbar</div>
