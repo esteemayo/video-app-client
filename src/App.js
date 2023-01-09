1c9bdfb0fe968633baf3807156b31e38a5850f55
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-09 01:52:35 +0100

    fix: add react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Menu from 'components/Menu';
 import Navbar from 'components/Navbar';
