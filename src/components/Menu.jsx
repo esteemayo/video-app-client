4391d45e09dc334d1e3ef52475cedc253e9a897f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-10 13:21:11 +0100

    fix: import useSelector

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import HomeIcon from '@mui/icons-material/Home';
 import ExploreOutlinedIcon from '@mui/icons-material/ExploreOutlined';
 import SubscriptionsOutlinedIcon from '@mui/icons-material/SubscriptionsOutlined';
@@ -20,6 +21,8 @@ import SettingsBrightnessOutlinedIcon from '@mui/icons-material/SettingsBrightne
 import logo from '../img/logo.png';
 
 const Menu = ({ darkMode, setDarkMode }) => {
+  const { user } = useSelector((state) => ({ ...state.user }));
+
   return (
     <Container>
       <Wrapper>
