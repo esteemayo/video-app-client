43278848921911cf11a2f680e8d238df63cc7490
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-10 14:25:12 +0100

    fix: add redux persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,13 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-import authSlice from 'features/user/userSlice';
-import videoSlice from 'features/video/videoSlice';
+import userReducer from 'features/user/userSlice';
+import videoReducer from 'features/video/videoSlice';
 
-const store = configureStore({
-  reducer: {
-    user: authSlice,
-    videos: videoSlice,
-  },
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  video: videoReducer,
 });
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+const persistor = persistStore(store);
+
+export { store, persistor };
