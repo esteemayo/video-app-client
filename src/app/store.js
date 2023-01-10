d192029027eea54846c8866555b0d44b000e94c1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-10 02:06:31 +0100

    fix: add authSlice to store

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+
+import authSlice from 'features/auth/authSlice';
 import videoSlice from 'features/video/videoSlice';
 
 const store = configureStore({
   reducer: {
+    auth: authSlice,
     videos: videoSlice,
   },
 });
