f223e5fe47da4262e0fdb48506d5f1f7b8fa0ff8
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-10 01:36:51 +0100

    fix: add password state variable

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 
 const SignIn = () => {
   const [username, setUsername] = useState(null);
+  const [password, setPassword] = useState(null);
+
   return (
     <Container>
       <Wrapper>
