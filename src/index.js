e8664b28abf6af70ef955a432a9816153fdbcc36
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-10 14:25:20 +0100

    fix: add redux persist

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import App from './App';
-import store from 'app/store';
+import { store } from 'app/store';
+import { persistor } from 'app/store';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
