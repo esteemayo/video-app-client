c8626dad2b2c62bee1a7c3774d1c27c7fbf5315f
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-11 15:25:42 +0100

    fix: import auth/provider from firebase config

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -4,6 +4,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
+import { auth, provider } from '../firebase';
 import { loginUser, reset } from 'features/user/userSlice';
 
 const SignIn = () => {
