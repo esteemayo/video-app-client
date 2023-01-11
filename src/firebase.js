440fcc22b53bd7e18e868d7b9d7d05599cdd4d54
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-11 15:15:29 +0100

    feat: add a firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCPR6bOQOP21LTueMCuZssgjTjTDhI28Jo',
+  authDomain: 'video-42829.firebaseapp.com',
+  projectId: 'video-42829',
+  storageBucket: 'video-42829.appspot.com',
+  messagingSenderId: '876716559497',
+  appId: '1:876716559497:web:e45a1168af541b42490a58'
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
