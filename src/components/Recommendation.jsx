b29abc65ca57cd079a0436d69e26df079e3be8a7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-13 16:09:08 +0100

    fix: fetch recommended videos

--- src/components/Recommendation.jsx
+++ src/components/Recommendation.jsx
@@ -3,10 +3,15 @@ import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Card from './Card';
+import { fetchRecommendedVideos } from 'features/video/videoSlice';
 
-const Recommendation = () => {
+const Recommendation = ({ tags }) => {
   const dispatch = useDispatch();
   const { recommendedVideos } = useSelector((state) => ({ ...state.video }));
+
+  useEffect(() => {
+    dispatch(fetchRecommendedVideos(tags));
+  }, [tags, dispatch]);
 
   return (
     <Container>
