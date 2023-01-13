c39955e9bd6fa87615a071b6199b2f6c75604980
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-13 04:14:45 +0100

    fix: change state values

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 const Upload = ({ onClose }) => {
   const [img, setImg] = useState(null);
   const [video, setVideo] = useState(null);
-  const [imgPerc, setImgPerc] = useState(null)
-  const [videoPerc, setVideoPerc] = useState(null);
+  const [imgPerc, setImgPerc] = useState(0)
+  const [videoPerc, setVideoPerc] = useState(0);
 
   return (
     <Container>
