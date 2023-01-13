9d7de48c7ce9a784bf635a9d8b48f97392025670
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-13 21:22:05 +0100

    feat: create a search video page

---
+++ src/pages/Search.jsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const Search = () => {
+  return (
+    <Container>Search</Container>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+`;
+
+export default Search;
