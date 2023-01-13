e5b31f1d058a5fcfa75e79a983e350f4b59d4b5a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-13 02:58:59 +0100

    fix: channel owner' image

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -78,7 +78,7 @@ const Video = () => {
         <Channel>
           <ChannelInfo>
             <Image
-              src='https://yt3.ggpht.com/yti/APfAmoE-Q0ZLJ4vk3vqmV4Kwp0sbrjxLyB8Q4ZgNsiRH=s88-c-k-c0x00ffffff-no-rj-mo'
+              src={video.user?.img}
               alt=''
             />
             <ChannelDetail>
