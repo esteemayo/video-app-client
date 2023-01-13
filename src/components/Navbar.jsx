a544fb5fbaa688294befffec29cdd283a14180c7
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-13 16:44:31 +0100

    fix: modify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,6 @@ const Navbar = () => {
 
   const [open, setOpen] = useState(true);
   const [query, setQuery] = useState('');
-  const { user } = useSelector((state) => ({ ...state.user }));
 
   const handleSearch = (e) => {
     e.preventDefault();
