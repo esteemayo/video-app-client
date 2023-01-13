3b6e638a8353a7f84e3a751f4c1c9629f1a0659a
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-13 21:26:56 +0100

    feat: import Card component

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { useDispatch, useSelector } from 'react-redux';
 
+import Card from 'components/Card';
+
 const Search = () => {
   const dispatch = useDispatch();
   const { videos } = useSelector((state) => ({ ...state.video }));
