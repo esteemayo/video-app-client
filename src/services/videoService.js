ac8f6c1d2c60dd74e7b2dd62925bfe4f741c9099
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-13 15:51:43 +0100

    feat: add new video api endpoint

--- src/services/videoService.js
+++ src/services/videoService.js
@@ -12,6 +12,9 @@ export const getTrendingVideos = () => http.get(`${apiEndpoint}/trend`);
 
 export const getSubscribedVideos = () => http.get(`${apiEndpoint}/subscribe`);
 
+export const getVideosByTags = (tags) =>
+  http.get(`${apiEndpoint}/tags?tags=${tags}`);
+
 export const getCommentsOnVideo = (videoId) =>
   http.get(`${videoUrl(videoId)}/comments`);
 
