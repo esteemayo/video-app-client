36b6660dcdacecd0a6651f0e509c1455cfaed7a6
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-14 02:04:48 +0100

    fix: modify

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
                 onClick={() => setOpen(false)}
               />
               <Avatar src={user.img} alt='' />
-              {user.username}
+              {user.name}
             </User>
           ) : (
             <Link to='signin' style={{ textDecoration: 'none' }}>
