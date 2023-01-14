d8b7bf1a49c03a407b4b8386e5ca9a18820e3876
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-14 02:52:56 +0100

    feat: add NotFound

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,15 @@
 import Home from './Home';
+import NotFound from './NotFound';
 import Search from './Search';
 import SharedLayout from './SharedLayout';
 import SignIn from './SignIn';
 import Video from './Video';
 
-export { Home, Search, SharedLayout, SignIn, Video };
+export {
+  Home,
+  NotFound,
+  Search,
+  SharedLayout,
+  SignIn,
+  Video,
+};
