81fa78ca40ac01953cd0275c4be76ce7fc9aaba0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-15 19:28:18 +0100

    fix: format

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -183,7 +183,12 @@ const SignIn = () => {
           placeholder='confirm password'
           onChange={handleChange}
         />
-        <Button disabled={perc > 0 && perc < 100} onClick={handleRegister}>Sign up</Button>
+        <Button
+          disabled={perc > 0 && perc < 100}
+          onClick={handleRegister}
+        >
+          Sign up
+        </Button>
       </Wrapper>
       <More>
         English(USA)
