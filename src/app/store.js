173ba691bb2667706f226b5588d3b371dcf7a58d
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-15 15:18:07 +0100

    fix: format

--- src/app/store.js
+++ src/app/store.js
@@ -25,7 +25,7 @@ const rootReducer = combineReducers({
   video: videoReducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
   reducer: persistedReducer,
