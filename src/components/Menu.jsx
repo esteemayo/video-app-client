1e69e0e4bdbcff3dcf33e8b83f296b47e3b32295
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-17 02:31:54 +0100

    fix: modify

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -138,7 +138,8 @@ const Menu = ({ darkMode, setDarkMode }) => {
 
 const Container = styled.div`
   flex: 1;
-  height: ${({ user }) => user ? '100vh' : '100%'};
+  height: 100vh;
+  min-height: ${({ user }) => user ? '100vh' : '103vh'};
   font-size: 1.4rem;
   background-color: ${({ theme }) => theme.bgLighter};
   color: ${({ theme }) => theme.text};
