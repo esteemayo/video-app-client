9d5751e93284d2137ad5957379a6a3151a5181b4
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-01-27 01:55:32 +0100

    feat: add convert

--- src/utils/index.js
+++ src/utils/index.js
@@ -18,3 +18,12 @@ export const excerpts = (str, count) => {
   }
   return str;
 }
+
+export function convert(value) {
+  if (value >= 1000000) {
+    value = (value / 1000000) + 'M'
+  } else if (value >= 1000) {
+    value = (value / 1000) + 'K';
+  }
+  return value;
+}
