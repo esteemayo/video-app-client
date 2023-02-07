a1d0c3c2c5cecb155f80560024c4a9fc0d17ff8b
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-07 14:10:08 +0100

    fix: modify

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
-import { useEffect } from 'react';
 import styled from 'styled-components';
-import { toast } from 'react-toastify';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { toast } from 'react-toastify';
 
 import Card from 'components/Card';
 import {
