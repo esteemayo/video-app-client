9b9545df2059c271591f0fee142c46f5c09319be
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-02-07 14:17:50 +0100

    fix: modify

--- src/components/Recommendation.jsx
+++ src/components/Recommendation.jsx
@@ -1,5 +1,5 @@
-import { useEffect } from 'react';
 import styled from 'styled-components';
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Card from './Card';
